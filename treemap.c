9dd9e5c9df3e2e23f152318f409b32769c4ab7fd
Your Name <you@example.com>
2024-05-10 01:53:03 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -81,7 +81,7 @@ void insertTreeMap(TreeMap * tree, void* key, void * value)
             }
             else
             {
-                if (aux->right == NULL);
+                if(aux->right == NULL)
                 {
                     aux->right = new;
                     new->parent = aux;
